cb2c51ed9ef42b8953b118ba3e0fac3d12fb058d
Luca <witt-luca@web.de>
2023-08-25 14:44:13 +0200

    Section on contexts reworded

--- formal-proofs/Resources/Monoid.lean
+++ formal-proofs/Resources/Monoid.lean
@@ -3,14 +3,46 @@ import Resources.Definitions
 open ResourceDemand
 
 --(0, 0) is left unit under sequencing
-theorem left_unit (p : ResourceDemand) : ((0, 0) ▹ p) = (p.init, p.resid) := by
-  simp[init]; simp[resid]
+theorem left_unit (p : ResourceDemand) : ((0, 0) ▹ (p.1, p.2)) = (p.1, p.2) := by
+  simp
 
 
 --(0, 0) is right unit under sequencing
-theorem right_unit (p: ResourceDemand) : (p ▹ (0, 0)) = p := by
-  simp[init]; simp[resid]
+theorem right_unit (p: ResourceDemand) : ((p.1, p.2) ▹ (0, 0)) = (p.1, p.2) := by
+  simp
 
 --sequencing is associative
 theorem associative (p q r : ResourceDemand) : (p ▹ (q ▹ r)) = ((p ▹ q) ▹ r) := by
-  simp[init]; simp[resid];
+  simp
+  . cases p.snd
+    . cases q.fst
+      . cases q.snd
+        . cases r.fst
+          . simp
+          . simp
+        . cases r.fst
+          . simp
+          . sorry
+      . cases q.snd
+        . cases r.fst
+          .simp
+          .simp[Nat.add_assoc]
+        . cases r.fst
+          . simp
+          . sorry
+    . cases q.fst
+      . cases q.snd
+        . cases r.fst
+          .simp
+          .simp
+        . cases r.fst
+          . simp[Nat.add_assoc]
+          . sorry
+      . cases q.snd
+        . cases r.fst
+          . sorry
+          . sorry
+        . cases r.fst
+          . sorry
+          . sorry
+  
